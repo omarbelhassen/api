dbc83db136a829c8c4f18f0799f9110ce2428d6e
omargit config --global user.name omargit config --global user.name omar <omarbelhassen16@gmail.com>
2020-12-07 14:28:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React , {useState , useEffect} from"react";
+import axios from "axios" ;
+import "./App.css";
+
+import "bootstrap/dist/bootstrap.min.css";
+import UserList from "./Components/UserList.js";
+
+function App () {
+  const [user , setUsers] = userState ([]) ;
+  const [error , setError] = useState (null) ;
+  const getUsers = () => {
+    axios
+    .get("https ://jsonplaceholder.typicode.com/users")
+  .then(function(reponse) {
+setUsers(reponse.data);
+  })
+  .catch(function(error){
+    setError(error);
+  });
+  };
+  userEffect (() => {
+    getUsers();
+
+  },[]);
+  console.log(error);
+  return (
+    <div className="App">
+      <div className="header"> User's List</div>
+      <div className="user-container">
+        <UserList data ={users}/>
+      </div>
+    </div>
+  );
+}
+export default App ;
